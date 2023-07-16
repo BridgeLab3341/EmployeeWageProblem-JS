8bb0b59bd85b60dbe59d2a1ec7e28e2b6e72fc41
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-16 17:10:18 +0530

    [somu]Add:Calculating Daily Employee Wage.

--- EmployeeWageProblem-JS.js
+++ EmployeeWageProblem-JS.js
@@ -1,11 +1,23 @@
-//UC1
-const Is_Absent=0;
-let empcheck=Math.floor(Math.random()*10)%2;
-if(empcheck == Is_Absent){
-    console.log("Employee is Absent");
-    return;
-}
-else
+//UC1,UC2
+//const Is_Absent=0;
+const Part_Time=1;
+const Full_Time=2;
+const Part_Time_Hour=4;
+const Full_Time_Hour=8;
+const Wage_Per_Hour=20;
+let empHrs=0;
+let empcheck=Math.floor(Math.random()*10) % 3;
+switch(empcheck)
 {
-    console.log("Employee is Present");
+    // case Is_Absent:
+    //     empHrs=0;
+    case Part_Time:
+        empHrs=Part_Time_Hour;
+    case Full_Time:
+        empHrs=Full_Time_Hour;
+    default:
+        empHrs=0;
+
 }
+let empWage=empHrs*Wage_Per_Hour;
+console.log("Employee Wage:- "+empWage);
