90d08770d17c89437c31661bc454dd9d4170550a
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-17 11:15:13 +0530

    [somu]Add:Storing Employee Wage Data in Array.

--- EmployeeWageProblem-JS.js
+++ EmployeeWageProblem-JS.js
@@ -1,4 +1,4 @@
-//UC1,UC2,UC3,UC4,UC5
+//UC1,UC2,UC3,UC4,UC5,UC6
 //const Is_Absent=0;
 const Part_Time=1;
 const Full_Time=2;
@@ -10,7 +10,7 @@ const Max_Hrs_In_Month=100;
 let totalEmpHrs=0;
 let totalWorkingDays=0;
 let empHrs=0;
-let empcheck=Math.floor(Math.random()*10) % 3;
+let empDailyWageArray=new Array();
 function GetWorkingHours(empcheck){
     switch(empcheck){
     case Part_Time:
@@ -21,9 +21,16 @@ function GetWorkingHours(empcheck){
         return 0;
 }  
 }
+function calcDailyWage(empHrs){
+    return empHrs*Wage_Per_Hour;
+}
 while(totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days){
     totalWorkingDays++;
-    totalEmpHrs+=GetWorkingHours(empcheck);
+    let empcheck=Math.floor(Math.random()*10) % 3; 
+   let empHrs=GetWorkingHours(empcheck);
+   totalEmpHrs+=empHrs;
+   empDailyWageArray.push(calcDailyWage(empHrs));
 }
-let empWage=totalEmpHrs * Wage_Per_Hour;
-console.log("Total Days:- "+totalWorkingDays+"\tTotal Hrs:- "+totalEmpHrs+"\tEmployee Wage:- "+empWage);
+let empDialyWage=calcDailyWage(totalEmpHrs);
+console.log("Total Days:- "+totalWorkingDays+"\tTotal Hrs:- "+totalEmpHrs+"\tEmployee Wage:- "+empDialyWage);
+console.log(empDailyWageArray);
